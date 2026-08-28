4410365f26e53cbda472cd22024cc400e4e03297
couvq <quentincouvelaire@gmail.com>
2026-08-28 16:16:21 -0400

    add endpoint test suite

---
+++ apps/server/src/controller/endpoint.controller.spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller.js';
+import { AppService } from '../service/app.service.js';
+import { EndpointController } from './endpoint.controller.js';
+import { EndpointService } from '../service/endpoint.service.js';
+import { EndpointRepository } from '../repository/endpoint.repository.js';
+
+describe('EndpointController', () => {
+  let endpointController: EndpointController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [EndpointController],
+      providers: [EndpointService, EndpointRepository],
+    }).compile();
+
+    endpointController = app.get<EndpointController>(EndpointController);
+  });
+
+  describe('root', () => {
+    it('should return empty array of endpoints initially', () => {
+      expect(endpointController.getAllEndpoints()).toEqual([]);
+    });
+
+    it('should create an endpoint', () => {
+      endpointController.addEndpoint({
+        method: 'GET',
+        path: 'test',
+        responseSchema: {},
+      });
+
+      const allEndpoints = endpointController.getAllEndpoints();
+      expect(allEndpoints.length).toBe(1);
+      const endpoint = allEndpoints[0]
+      const endpointId = endpoint.id;
+
+      const matchingEndpoint = endpointController.getEndpointById(endpointId)
+      expect(matchingEndpoint).toEqual(endpoint)
+    });
+  });
+});
