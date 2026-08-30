f80eb9b4ddb1a3e755025cf0dbabe487f0819bc8
couvq <quentincouvelaire@gmail.com>
2026-08-30 13:46:30 -0400

    fix migrate issue

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
-import { db } from './clients/endpointDbClient.js';
 
 async function bootstrap() {
-  migrate(db, { migrationsFolder: './drizzle' });
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks()
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
