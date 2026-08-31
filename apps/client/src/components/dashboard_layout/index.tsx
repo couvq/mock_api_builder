3133694f8ecd391093f1e34c1e399d69013ec3f1
couvq <quentincouvelaire@gmail.com>
2026-08-31 17:32:02 -0400

    update layout to be more responsive

--- apps/client/src/components/dashboard_layout/index.tsx
+++ apps/client/src/components/dashboard_layout/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack } from "@mui/material";
+import { Box, Paper, Stack } from "@mui/material";
 import type { ReactNode } from "react";
 
 interface DashboardLayoutProps {
@@ -13,13 +13,15 @@ const DashboardLayout = ({
   responsePanel,
 }: DashboardLayoutProps) => {
   return (
-    <Stack direction="row" spacing={1}>
-      <Paper>{sidePanel}</Paper>
-      <Stack direction="column" spacing={1}>
-        <Paper>{editorPanel}</Paper>
-        <Paper>{responsePanel}</Paper>
-      </Stack>
-    </Stack>
+    <Box sx={{ maxHeight: "100vh", maxWidth: "100vw" }}>
+        <Stack direction="row" spacing={1}>
+          <Paper>{sidePanel}</Paper>
+          <Stack direction="column" spacing={1} sx={{ width: "100%", padding: 1.5 }}>
+            <Paper>{editorPanel}</Paper>
+            <Paper>{responsePanel}</Paper>
+          </Stack>
+        </Stack>
+    </Box>
   );
 };
 
