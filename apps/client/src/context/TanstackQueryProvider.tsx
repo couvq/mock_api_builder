253eb57f5f78ee4a1d72f78944fc2c5f1fa94099
couvq <quentincouvelaire@gmail.com>
2026-09-02 10:03:56 -0400

    make error toast the global default for tanstack uncaught errors

--- apps/client/src/context/TanstackQueryProvider.tsx
+++ apps/client/src/context/TanstackQueryProvider.tsx
@@ -1,11 +1,22 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { enqueueSnackbar } from "notistack";
 import type { ReactNode } from "react";
 
 interface TanstackQueryProviderProps {
   children: ReactNode;
 }
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error) => {
+      enqueueSnackbar(error.message, { variant: "error" });
+    },
+  }),
+});
 
 const TanstackQueryProvider = ({ children }: TanstackQueryProviderProps) => {
   return (
